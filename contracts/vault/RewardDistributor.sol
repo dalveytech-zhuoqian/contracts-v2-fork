3f3c0be779bff9d938d9e857cfa66bb51c38a7a5
Zhuoqian Zhou <zhuoqian.zhou@dalveytech.com>
2024-03-15 02:21:55 +0800

    modified:   contracts/vault/RewardDistributor.sol

--- contracts/vault/RewardDistributor.sol
+++ contracts/vault/RewardDistributor.sol
@@ -1,35 +1,48 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
+import {AccessManagedUpgradeable} from "@openzeppelin/contracts-upgradeable/access/manager/AccessManagedUpgradeable.sol";
+
+import {IVaultReward} from "../interfaces/IVaultReward.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import {IVaultReward} from "./interfaces/IVaultReward.sol";
-import "../ac/Ac.sol";
 
-contract RewardDistributor is AcUpgradable {
+contract RewardDistributor is AccessManagedUpgradeable {
     using SafeERC20 for IERC20;
 
-    address public rewardToken;
-    uint256 public tokensPerInterval;
-    uint256 public lastDistributionTime;
-    address public rewardTracker;
+    bytes32 constant POS_STORAGE_POSITION = keccak256("blex.reward.distributor.storage");
+
+    struct StorageStruct {
+        address rewardToken;
+        uint256 tokensPerInterval;
+        uint256 lastDistributionTime;
+        address rewardTracker;
+    }
+
+    function _getStorage() private pure returns (StorageStruct storage $) {
+        bytes32 position = POS_STORAGE_POSITION;
+        assembly {
+            $.slot := position
+        }
+    }
 
     event Distribute(uint256 amount);
     event TokensPerIntervalChange(uint256 amount);
+    /**
+     * @dev Modifier to only allow the reward tracker contract to call a function.
+     */
 
-    // constructor() Ac(msg.sender) {}
+    modifier onlyRewardTracker() {
+        require(msg.sender == _getStorage().rewardTracker, "RewardDistributor: invalid msg.sender");
+        _;
+    }
 
-    function initialize(
-        address _rewardToken,
-        address _rewardTracker
-    ) external initializer {
-        AcUpgradable._initialize(msg.sender);
+    function initialize(address _rewardToken, address _rewardTracker) external onlyInitializing {
         require(_rewardToken != address(0));
         require(_rewardTracker != address(0));
-        rewardToken = _rewardToken;
-        rewardTracker = _rewardTracker;
+        _getStorage().rewardToken = _rewardToken;
+        _getStorage().rewardTracker = _rewardTracker;
     }
 
     /**
@@ -39,11 +52,7 @@ contract RewardDistributor is AcUpgradable {
      * @param _account The address to transfer the tokens to.
      * @param _amount The amount of tokens to withdraw.
      */
-    function withdrawToken(
-        address _token,
-        address _account,
-        uint256 _amount
-    ) external onlyAdmin {
+    function withdrawToken(address _token, address _account, uint256 _amount) external restricted {
         IERC20(_token).safeTransfer(_account, _amount);
     }
 
@@ -51,8 +60,8 @@ contract RewardDistributor is AcUpgradable {
      * @dev Updates the last distribution time to the current block timestamp.
      * Only the admin can call this function.
      */
-    function updateLastDistributionTime() external onlyRole(VAULT_MGR_ROLE) {
-        lastDistributionTime = block.timestamp;
+    function updateLastDistributionTime() external restricted {
+        _getStorage().lastDistributionTime = block.timestamp;
     }
 
     /**
@@ -60,43 +69,17 @@ contract RewardDistributor is AcUpgradable {
      * Only the admin can call this function.
      * @param _amount The number of tokens per interval.
      */
-    function setTokensPerInterval(
-        uint256 _amount
-    ) external onlyRole(VAULT_MGR_ROLE) {
+    function setTokensPerInterval(uint256 _amount) external restricted {
         /* 
         require(
             lastDistributionTime != 0,
             "RewardDistributor: invalid lastDistributionTime"
         );
         */
-        if (lastDistributionTime == 0) lastDistributionTime = block.timestamp;
-        IVaultReward(rewardTracker).updateRewards();
-        tokensPerInterval = _amount;
+        if (_getStorage().lastDistributionTime == 0) _getStorage().lastDistributionTime = block.timestamp;
+        IVaultReward(_getStorage().rewardTracker).updateRewards();
+        _getStorage().tokensPerInterval = _amount;
         emit TokensPerIntervalChange(_amount);
-    }
-
-    /**
-     * @dev Calculates the pending rewards based on the last distribution time and tokens per interval.
-     * @return The pending rewards.
-     */
-    function pendingRewards() public view returns (uint256) {
-        if (block.timestamp == lastDistributionTime) {
-            return 0;
-        }
-
-        uint256 timeDiff = block.timestamp - lastDistributionTime;
-        return tokensPerInterval * timeDiff;
-    }
-
-    /**
-     * @dev Modifier to only allow the reward tracker contract to call a function.
-     */
-    modifier onlyRewardTracker() {
-        require(
-            msg.sender == rewardTracker,
-            "RewardDistributor: invalid msg.sender"
-        );
-        _;
     }
 
     /**
@@ -110,18 +93,32 @@ contract RewardDistributor is AcUpgradable {
             return 0;
         }
 
-        lastDistributionTime = block.timestamp;
+        _getStorage().lastDistributionTime = block.timestamp;
 
-        uint256 balance = IERC20(rewardToken).balanceOf(address(this));
+        uint256 balance = IERC20(_getStorage().rewardToken).balanceOf(address(this));
         if (amount > balance) {
             amount = balance;
         }
 
-        IERC20(rewardToken).safeTransfer(msg.sender, amount);
+        IERC20(_getStorage().rewardToken).safeTransfer(msg.sender, amount);
 
         emit Distribute(amount);
         return amount;
     }
+    //====================================================================================
+    //    view functions
+    //====================================================================================
 
-    uint256[50] private ______gap;
+    /**
+     * @dev Calculates the pending rewards based on the last distribution time and tokens per interval.
+     * @return The pending rewards.
+     */
+    function pendingRewards() public view returns (uint256) {
+        if (block.timestamp == _getStorage().lastDistributionTime) {
+            return 0;
+        }
+
+        uint256 timeDiff = block.timestamp - _getStorage().lastDistributionTime;
+        return _getStorage().tokensPerInterval * timeDiff;
+    }
 }
