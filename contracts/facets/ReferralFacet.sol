d6a062d1d38fc3cd2abf8fbcb40d4f426464e469
Zhuoqian Zhou <zhuoqian.zhou@dalveytech.com>
2024-03-14 14:09:31 +0800

    fix imports in referral facets

--- contracts/facets/ReferralFacet.sol
+++ contracts/facets/ReferralFacet.sol
@@ -2,39 +2,39 @@
 pragma solidity ^0.8.17;
 
 import {LibAccessManaged} from "../lib/ac/LibAccessManaged.sol";
-import {LibReferral} from "../lib/referral/LibReferral.sol";
+import {ReferralHandler} from "../lib/referral/ReferralHandler.sol";
 
 contract ReferralFacet {
     function setTier(uint256 _tierId, uint256 _totalRebate, uint256 _discountShare) external restricted {
-        LibReferral.setTier(_tierId, _totalRebate, _discountShare);
+        ReferralHandler.setTier(_tierId, _totalRebate, _discountShare);
     }
 
     function setReferrerTier(address _referrer, uint256 _tierId) external restricted {
-        LibReferral.setReferrerTier(_referrer, _tierId);
+        ReferralHandler.setReferrerTier(_referrer, _tierId);
     }
 
     function setReferrerDiscountShare(address _account, uint256 _discountShare) external restricted {
-        LibReferral.setReferrerDiscountShare(_account, _discountShare);
+        ReferralHandler.setReferrerDiscountShare(_account, _discountShare);
     }
 
     function setTraderReferralCode(address _account, bytes32 _code) external restricted {
-        LibReferral._setTraderReferralCode(_account, _code);
+        ReferralHandler._setTraderReferralCode(_account, _code);
     }
 
     function setTraderReferralCodeByUser(bytes32 _code) external {
-        LibReferral._setTraderReferralCode(msg.sender, _code);
+        ReferralHandler._setTraderReferralCode(msg.sender, _code);
     }
 
     function registerCode(bytes32 _code) external {
-        LibReferral.registerCode(_code);
+        ReferralHandler.registerCode(_code);
     }
 
     function setCodeOwner(bytes32 _code, address _newAccount) external {
-        LibReferral.setCodeOwner(_code, _newAccount);
+        ReferralHandler.setCodeOwner(_code, _newAccount);
     }
 
     function govSetCodeOwner(bytes32 _code, address _newAccount) external restricted {
-        LibReferral.govSetCodeOwner(_code, _newAccount);
+        ReferralHandler.govSetCodeOwner(_code, _newAccount);
     }
 
     //========================================================================
@@ -42,10 +42,10 @@ contract ReferralFacet {
     //========================================================================
 
     function getTraderReferralInfo(address _account) internal view returns (bytes32, address) {
-        return LibReferral.getTraderReferralInfo(_account);
+        return ReferralHandler.getTraderReferralInfo(_account);
     }
 
     function getCodeOwners(bytes32 _code) external view returns (address) {
-        return LibReferral.codeOwners(_code);
+        return ReferralHandler.codeOwners(_code);
     }
 }
