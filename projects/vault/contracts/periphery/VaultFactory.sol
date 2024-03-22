3ae0480b6731c123d40788e84d49606a938a909d
Charlie <delveytech-zhuoqian@dalveytech.com>
2024-03-22 11:27:38 +0800

    update vault factory

--- projects/vault/contracts/periphery/VaultFactory.sol
+++ projects/vault/contracts/periphery/VaultFactory.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {AccessManagedUpgradeable} from "@openzeppelin/contracts-upgradeable/access/manager/AccessManagedUpgradeable.sol";
-import {Vault} from "./Vault.sol";
+import {Vault} from "../vault/Vault.sol";
 
 contract VaultFactory is AccessManagedUpgradeable {
     struct Parameters {
@@ -13,6 +13,8 @@ contract VaultFactory is AccessManagedUpgradeable {
     }
 
     Parameters public parameters;
+    uint256 public vaultCount;
+    address[] public vaults;
 
     event NewVault(address indexed vault, address indexed asset, address indexed market, string name, string symbol);
 
@@ -24,6 +26,9 @@ contract VaultFactory is AccessManagedUpgradeable {
     /// @param _user: Cake pool user
     /// @return proxy The proxy address
     function deploy(Parameters calldata p) external restricted returns (address proxy) {
+        // TODO
+        // 1. 做到可升级
+        // 2. 一个升级, 全部升级
         parameters = p;
         vault = new Vault{salt: keccak256(abi.encode(
             p.name, 
@@ -33,5 +38,12 @@ contract VaultFactory is AccessManagedUpgradeable {
         vault.initialize(p.asset, p.market, p.name, p.symbol, authority());
         delete parameters;
         emit NewVault(address(vault), p.asset, p.market, p.name, p.symbol);
+    }
+
+    function upgradeTo(address _implementation) external restricted {
+        for (uint256 i = 0; i < vaultCount; i++) {
+            Vault vault = Vault(vaults[i]);
+            // vault.upgradeTo(_implementation);
+        }
     }
 }
