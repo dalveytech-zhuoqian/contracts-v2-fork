48bc7b31bd4ab3e095cdbc8997900d2d93c52339
Charlie <delveytech-zhuoqian@dalveytech.com>
2024-04-01 11:39:23 +0800

    Revert "Add new files and update configurations"
    This reverts commit 2b0909dec0f6d420a69058e79aed158a90fbf3ce.

--- projects/vault/hardhat.config.ts
+++ projects/vault/hardhat.config.ts
@@ -1,97 +1,21 @@
-import 'dotenv/config'
-import { HardhatUserConfig } from 'hardhat/types'
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
 
-import '@nomicfoundation/hardhat-chai-matchers'
-import '@nomicfoundation/hardhat-ethers'
-import '@typechain/hardhat'
-import 'hardhat-gas-reporter'
-import 'solidity-coverage'
-
-import 'hardhat-deploy'
-import 'hardhat-deploy-ethers'
-import 'hardhat-deploy-tenderly'
-
-import { node_url, accounts, addForkConfiguration } from './utils/network'
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
-	solidity: {
-		compilers: [
-			{
-				version: '0.8.20',
-				settings: {
-					optimizer: {
-						enabled: false,
-						runs: 1,
-					},
-				},
-			},
-		],
-	},
-	namedAccounts: {
-		deployer: 0,
-		simpleERC20Beneficiary: 1,
-	},
-	networks: addForkConfiguration({
-		hardhat: {
-			initialBaseFeePerGas: 0, // to fix : https://github.com/sc-forks/solidity-coverage/issues/652, see https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136
-		},
-		localhost: {
-			url: node_url('localhost'),
-			accounts: accounts(),
-		},
-		staging: {
-			url: node_url('rinkeby'),
-			accounts: accounts('rinkeby'),
-		},
-		production: {
-			url: node_url('mainnet'),
-			accounts: accounts('mainnet'),
-		},
-		mainnet: {
-			url: node_url('mainnet'),
-			accounts: accounts('mainnet'),
-		},
-		sepolia: {
-			url: node_url('sepolia'),
-			accounts: accounts('sepolia'),
-		},
-		kovan: {
-			url: node_url('kovan'),
-			accounts: accounts('kovan'),
-		},
-		goerli: {
-			url: node_url('goerli'),
-			accounts: accounts('goerli'),
-		},
-	}),
-	paths: {
-		sources: 'src',
-	},
-	gasReporter: {
-		currency: 'USD',
-		gasPrice: 100,
-		enabled: process.env.REPORT_GAS ? true : false,
-		coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-		maxMethodDiff: 10,
-	},
-	mocha: {
-		timeout: 0,
-	},
-	external: process.env.HARDHAT_FORK
-		? {
-			deployments: {
-				// process.env.HARDHAT_FORK will specify the network that the fork is made from.
-				// these lines allow it to fetch the deployments from the network being forked from both for node and deploy task
-				hardhat: ['deployments/' + process.env.HARDHAT_FORK],
-				localhost: ['deployments/' + process.env.HARDHAT_FORK],
-			},
-		}
-		: undefined,
+  solidity: "0.8.24",
+  defender: {
+    apiKey: process.env.DEFENDER_KEY as string,
+    apiSecret: process.env.DEFENDER_SECRET as string,
+  },
+  networks: {
+    sepolia: {
+      url: "https://ethereum-sepolia.publicnode.com",
+      chainId: 11155111
+    },
+  },
+};
 
-	tenderly: {
-		project: 'template-ethereum-contracts',
-		username: process.env.TENDERLY_USERNAME as string,
-	},
-}
-
-export default config
+export default config;
