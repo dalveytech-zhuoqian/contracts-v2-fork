f91b4eed690dc759ca878c0939b9b0f2fe159979
Charlie <delveytech-zhuoqian@dalveytech.com>
2024-04-14 16:04:27 +0800

    test:percentage math

--- projects/market/src/lib/utils/test_PercentageMath.sol
+++ projects/market/src/lib/utils/test_PercentageMath.sol
@@ -26,4 +26,44 @@ contract PercentageMathTest is Test {
     function testFailValidMax() public {
         PercentageMath.valid(1e4 + 1);
     }
+
+    function testPercentMul() public {
+        uint256 value = 100;
+        uint256 percentage = 0.5e4;
+        uint256 expected = 50;
+        uint256 result = PercentageMath.percentMul(value, percentage);
+        assertEq(result, expected, "Incorrect percentage multiplication result");
+
+        value = 200;
+        percentage = 0.75e4;
+        expected = 150;
+        result = PercentageMath.percentMul(value, percentage);
+        assertEq(result, expected, "Incorrect percentage multiplication result");
+
+        value = 0;
+        percentage = 1e4;
+        expected = 0;
+        result = PercentageMath.percentMul(value, percentage);
+        assertEq(result, expected, "Incorrect percentage multiplication result");
+    }
+
+    function testPercentDiv() public {
+        uint256 value = 100;
+        uint256 percentage = 0.5e4;
+        uint256 expected = 200;
+        uint256 result = PercentageMath.percentDiv(value, percentage);
+        assertEq(result, expected, "Incorrect percentage division result");
+
+        value = 200;
+        percentage = 0.2e4;
+        expected = 1000;
+        result = PercentageMath.percentDiv(value, percentage);
+        assertEq(result, expected, "Incorrect percentage division result");
+
+        value = 0;
+        percentage = 1e4;
+        expected = 0;
+        result = PercentageMath.percentDiv(value, percentage);
+        assertEq(result, expected, "Incorrect percentage division result");
+    }
 }
