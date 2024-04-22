2614f367844b40809abedc078523d7b0c71293cb
Charlie <zhuoqian.zhou@dalveytech.com>
2024-04-22 16:54:11 +0800

    Refactor mint function to accept uriList parameter in issue.ts

--- projects/NFT/scripts/issue.ts
+++ projects/NFT/scripts/issue.ts
@@ -2,18 +2,19 @@ import { ethers,deployments,getNamedAccounts } from "hardhat";
 import { BlexSBT } from "../typechain-types";
 import { waitFor } from "../utils/wait";
 
-async function mint(addressList: string[]) {
+async function mint(addressList: string[],uriList: string[]) {
   const { deployer } = await getNamedAccounts();
   console.log(deployer);
   const targetContract = await ethers.getContract<BlexSBT>("BlexSBT");
-  await waitFor(targetContract.connect(await ethers.getSigner(deployer)).issueDegrees(addressList));
+  await waitFor(targetContract.connect(await ethers.getSigner(deployer)).issueDegreeList(addressList,uriList));
   console.log("mint success");
 }
 
 async function main() {
   // read mint list from file
-  const addressList = ["0xEa985d6ae63F26b1d3246AD81c5B01c2E6239966"];
-  await mint(addressList);
+  const addressList = ["0xEa985d6ae63F26b1d3246AD81c5B01c2E6239966","0xD2A47d453a6203f0B6b14e41d0b3B63E1C129A9f"];
+  const uriList = ["1","2"]
+  await mint(addressList,uriList);
 }
 
 main()
